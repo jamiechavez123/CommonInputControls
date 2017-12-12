0ca7728ea94a594c5262283d005a20e48771156f
jamie <jchavez@lincolnschool.org>
2017-12-12 14:01:27 -0500

    Final Submission

--- CommonInputControls/ViewController.swift
+++ CommonInputControls/ViewController.swift
@@ -15,11 +15,42 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+
+
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        print("Button was tapped")
+    }
+    
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        print("The slider is set to \(sender.value)")
+        
+    }
+    @IBAction func switchToggled(_ sender: UISwitch) {
+        if sender.isOn{
+            print("The switch is On!")
+        } else {
+            print("The switch is Off!")
+        }
+    }
+
+   
+    @IBAction func keyboardReturnKeyTapped(_ sender: UITextField) {
+    }
+    
+    @IBAction func textChanged(_ sender: UITextField) {
+        if let text = sender.text{
+        print(text)
+        }
+    }
+
+    @IBAction func respondToTapGesture(_ sender: UITapGestureRecognizer) {
+        let location = sender.location(in: view)
+        print(location)
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
 }
 
